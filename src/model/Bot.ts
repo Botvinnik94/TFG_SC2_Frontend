eac7bebc35182b155e5d5cb6acabad9d3513734c
Gabino Luis <gabinoluis@hotmail.es>
2020-03-30 16:37:44 +0200

    Add attribute script for Bot model.
    Script is the url where the python program is stored.

--- src/model/Bot.ts
+++ src/model/Bot.ts
@@ -3,14 +3,16 @@ export class Bot {
     id: string | undefined;
     name: string;
     uid: string;
+    script: string;
 
-    protected constructor(name: string, uid: string) {
+    protected constructor(name: string, uid: string, script: string) {
         this.name = name;
         this.uid = uid;
+        this.script = script;
     }
 
-    static build(name: string, uid: string, id?: string){
-        const bot = new Bot(name, uid);
+    static build(name: string, uid: string, script: string, id?: string){
+        const bot = new Bot(name, uid, script);
         bot.id = id;
         return bot;
     }
