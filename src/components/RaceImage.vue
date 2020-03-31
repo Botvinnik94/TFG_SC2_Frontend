32fda22997c04997a9aa4ae30f2f892c86178390
Gabino Luis <gabinoluis@hotmail.es>
2020-03-31 19:30:16 +0200

    Add component for selecting the race icon

---
+++ src/components/RaceImage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="race">
+        <v-img
+            v-if="race == 'Terran'"
+            class="shrink mr-2"
+            contain
+            src="@/assets/terran.png"
+            transition="scale-transition"
+            width="40"
+        ></v-img>
+
+        <v-img
+            v-if="race == 'Zerg'"
+            class="shrink mr-2"
+            contain
+            src="@/assets/zerg.png"
+            transition="scale-transition"
+            width="40"
+        ></v-img>
+
+        <v-img
+            v-if="race == 'Protoss'"
+            class="shrink mr-2"
+            contain
+            src="@/assets/protoss.png"
+            transition="scale-transition"
+            width="40"
+        ></v-img>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+    name: "RaceImage",
+    props: ['race']
+})
+</script>
